c47f9f87f8413ce88d00db3f6851bc3dc88b3a3d
James D. Nurmi <james@auspicious.abneptis.net>
2010-10-30 13:13:58 -0700

    add push/pop functionality

--- sqs/queue.go
+++ sqs/queue.go
@@ -1,11 +1,16 @@
 package sqs
 
 import "com.abneptis.oss/goaws"
+import "com.abneptis.oss/uuid"
 //import "log"
+import "bytes"
 import "http"
 import "os"
 import "path"
+import "strconv"
 import "xml"
+
+var MaxNumberOfMessages = 10
 
 type Queue struct {
   Name string
@@ -43,6 +48,88 @@ func (self *Queue)Delete(id goaws.Signer)(err os.Error){
       parser := xml.NewParser(resp.Body)
       xresp := createQueueResponse{}
       err = parser.Unmarshal(&xresp, nil)
+    } else {
+      err = os.NewError("Received unexpected status: " + resp.Status)
+    }
+  }
+  return
+}
+
+type sendMessageResponse struct {
+  SendMessageResult  sendMessageResult
+}
+
+type sendMessageResult struct {
+  MD5OfMessageBody string
+  MessageId string
+}
+
+
+// NB, we don't do any verification of the MD5
+func (self *Queue)Push(id goaws.Signer, body []byte)(msgid *uuid.UUID, err os.Error){
+   sqsReq, err := NewSQSRequest(map[string]string{
+    "Action": "SendMessage",
+    "AWSAccessKeyId": string(id.PublicIdentity()),
+    "MessageBody": string(body),
+  })
+  if err != nil { return }
+  resp, err := SignAndSendSQSRequest(id, "GET", self.URL, self.ProxyURL, &sqsReq)
+  //bb, _ := http.DumpResponse(resp, true)
+  //os.Stdout.Write(bb)
+  if err == nil {
+    if resp.StatusCode == 200 {
+      parser := xml.NewParser(resp.Body)
+      xresp := sendMessageResponse{}
+      err = parser.Unmarshal(&xresp, nil)
+      if err == nil { msgid, err = uuid.Parse(xresp.SendMessageResult.MessageId) }
+    } else {
+      err = os.NewError("Received unexpected status: " + resp.Status)
+    }
+  }
+  return
+}
+
+func (self *Queue)PushString(id goaws.Signer, body string)(*uuid.UUID, os.Error){
+  buff := bytes.NewBufferString(body)
+  return self.Push(id, buff.Bytes())
+}
+
+type receiveMessageResponse struct {
+  ReceiveMessageResult receiveMessageResult
+}
+
+type receiveMessageResult struct {
+  Message []*rawMessage
+}
+
+func (self *Queue)FetchMessages(id goaws.Signer, lim, timeout int)(m []*Message, err os.Error){
+  if lim <= 0 { lim = MaxNumberOfMessages }
+   sqsReq, err := NewSQSRequest(map[string]string{
+    "Action": "ReceiveMessage",
+    "AWSAccessKeyId": string(id.PublicIdentity()),
+    "AttributeName": "All",
+    "MaxNumberOfMessages": strconv.Itoa(lim),
+  })
+  if err != nil { return }
+  if timeout >= 0 {
+    err = sqsReq.Set("VisibilityTimeout", strconv.Itoa(timeout))
+    if err != nil { return }
+  }
+  resp, err := SignAndSendSQSRequest(id, "GET", self.URL, self.ProxyURL, &sqsReq)
+  //bb, _ := http.DumpResponse(resp, true)
+  //os.Stdout.Write(bb)
+  if err == nil {
+    if resp.StatusCode == 200 {
+      parser := xml.NewParser(resp.Body)
+      xresp := receiveMessageResponse{}
+      err = parser.Unmarshal(&xresp, nil)
+      if err == nil {
+        m = make([]*Message, len(xresp.ReceiveMessageResult.Message))
+        for mi := range(xresp.ReceiveMessageResult.Message) {
+          m[mi], err = xresp.ReceiveMessageResult.Message[mi].Message()
+          if err != nil { break }
+        }
+      }
     } else {
       err = os.NewError("Received unexpected status: " + resp.Status)
     }
