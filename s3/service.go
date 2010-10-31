6a7e9816d7b9e214ea15be29f5839c29c2c65d86
James D. Nurmi <james@abneptis.com>
2010-10-31 12:32:33 -0700

    Remove debugging

--- s3/service.go
+++ s3/service.go
@@ -5,7 +5,7 @@ import "com.abneptis.oss/aws/auth"
 //import "com.abneptis.oss/aws"
 
 import "os"
-import "http"
+//import "http"
 
 //<ListAllMyBucketsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Owner><ID>19f48e038756359c402c774f40ea9b193668d906b8836c783823b9fd33b270ef</ID><DisplayName>amazon</DisplayName></Owner><Buckets><Bucket><Name>records.abneptis.com</Name><CreationDate>2010-10-31T18:51:43.000Z</CreationDate></Bucket><Bucket><Name>records.abneptis.net</Name><CreationDate>2010-10-31T18:51:56.000Z</CreationDate></Bucket></Buckets></ListAllMyBucketsResult>
 
@@ -36,15 +36,23 @@ func ListBuckets(id auth.Signer, ep *awsconn.Endpoint)(out []string, err os.Erro
   hreq, err := req.HTTPRequest(id, ep, "","")
   if err != nil { return }
 
-  bb, _ := http.DumpRequest(hreq, true)
-  os.Stdout.Write(bb)
+  //bb, _ := http.DumpRequest(hreq, true)
+  //os.Stdout.Write(bb)
 
   cconn, err := ep.NewHTTPClientConn("tcp", "", nil)
   if err != nil { return }
   defer cconn.Close()
   resp, err := awsconn.SendRequest(cconn, hreq)
+  if err != nil { return }
+  result := &listBucketsResult{}
+  err = awsconn.ParseResponse(resp, result)
+  if err != nil { return }
+  out = make([]string, len(result.Buckets))
+  for i := range(result.Buckets){
+    out[i] = result.Buckets[i].Bucket.Name
+  }
 
-  bb, _ = http.DumpResponse(resp, true)
-  os.Stdout.Write(bb)
+  //bb, _ = http.DumpResponse(resp, true)
+  //os.Stdout.Write(bb)
   return
 }
