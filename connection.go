117b6ee6b340ad55632fa02d5a3171fd711763f3
James D. Nurmi <james@abneptis.com>
2010-10-30 18:26:42 -0700

    Add godoc

--- connection.go
+++ connection.go
@@ -7,6 +7,10 @@ package aws
 import "http"
 import "os"
 
+/*
+  Simple wrapper that does an http.ClientConn Write/Read and returns
+  the response/errors upstream as appropriate.
+*/
 func SendRequest(cc *http.ClientConn, req *http.Request)(resp *http.Response, err os.Error){
   err = cc.Write(req)
   if err == nil {
