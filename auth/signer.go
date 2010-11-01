0d956ec5f8d0d1b44e258497f1b2eb7a901b8414
James D. Nurmi <james@abneptis.com>
2010-11-01 11:26:27 -0700

    Add signable interface

--- auth/signer.go
+++ auth/signer.go
@@ -18,12 +18,19 @@ package auth
 
 import "com.abneptis.oss/cryptools/signer"
 
+import "os"
+
 // The signer interface extends the cryptools signer interface by
 // requiring a PublicIdentity() function that returns a []byte array
 // that identifies the signing party.
 type Signer interface {
   signer.Signer
   PublicIdentity()([]byte)
+}
+
+// A signable object must implement a canonical string.
+type Signable interface {
+  Sign(Signer)(os.Error)
 }
 
 // Wraps the auth.Signer interface by returning the PublicIdentity
