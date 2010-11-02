e62b5d6bdf03eb6327819b70b8bc8dc801af1411
James D. Nurmi <james@abneptis.com>
2010-11-01 17:57:51 -0700

    Fix timeout logic, update godoc

--- sqs/queue.go
+++ sqs/queue.go
@@ -20,16 +20,22 @@ type Queue struct {
   Endpoint *awsconn.Endpoint
 }
 
+
+// Creates a new queue from a name and endpoint.
 func NewQueue(n string, ep *awsconn.Endpoint)(*Queue){
   return &Queue{Name:n, Endpoint: ep}
 }
 
+// Creates a queue by parsing the endpoint URL.
+// This could potentially break down the road if
+// amazon ever changes their SQS URI format substantially.
 func NewQueueURL(ep *awsconn.Endpoint)(mq *Queue){
   _, name := path.Split(ep.URL.Path)
   mq = NewQueue(name, ep)
   return
 }
 
+// Deletes an SQS queue.
 func (self *Queue)Delete(id auth.Signer)(err os.Error){
   if self == nil || self.Endpoint == nil { return os.NewError("Undefined endpoint!") }
   sqsReq, err := self.signedRequest(id, map[string]string{
@@ -45,6 +51,8 @@ func (self *Queue)Delete(id auth.Signer)(err os.Error){
   return
 }
 
+// Pushes the raw bytes into SQS, returning the message UUID.
+//
 // NB, we don't do any verification of the MD5
 func (self *Queue)Push(id auth.Signer, body []byte)(msgid *uuid.UUID, err os.Error){
   sqsReq, err := self.signedRequest(id, map[string]string{
@@ -61,18 +69,24 @@ func (self *Queue)Push(id auth.Signer, body []byte)(msgid *uuid.UUID, err os.Err
   return
 }
 
+// Helper function that Push()'s a string instead of []bytes.
 func (self *Queue)PushString(id auth.Signer, body string)(*uuid.UUID, os.Error){
   buff := bytes.NewBufferString(body)
   return self.Push(id, buff.Bytes())
 }
 
+// Fetches messages from SQS.  Note SQS may raise an error if you
+// set "lim" higher than 10 (lim <0 will default to MaxNumberOfMessages.).
+// Timeout is 
 func (self *Queue)FetchMessages(id auth.Signer, lim, timeout int)(m []*Message, err os.Error){
   if lim <= 0 { lim = MaxNumberOfMessages }
-  sqsReq, err := self.signedRequest(id, map[string]string{
+  parms := map[string]string{
     "Action": "ReceiveMessage",
     "AttributeName": "All",
     "MaxNumberOfMessages": strconv.Itoa(lim),
-  })
+  }
+  if timeout >= 0 { parms["Timeout"] = strconv.Itoa(timeout) }
+  sqsReq, err := self.signedRequest(id, parms)
   if err != nil { return }
   xresp := &receiveMessageResponse{}
   xerr := &errorResponse{}
