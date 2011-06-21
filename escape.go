973dc237f6df03d79f3569ffec1653cf4673f717
James D. Nurmi <james@abneptis.com>
2011-06-20 23:01:45 -0700

    bring back escape functionality (requried for SDB to operate still)

--- escape.go
+++ escape.go
@@ -1,6 +1,10 @@
 package aws
 
 import (
+  "com.abneptis.oss/urltools"
+)
+
+import (
   "http"
   "sort"
 )
@@ -16,7 +20,27 @@ func SortedEscape(v http.Values)(out string){
     if k > 0 {
       out += "&"
     }
-    out += http.URLEscape(keys[k]) + "=" + http.URLEscape(v.Get(keys[k]))
+    // out += http.URLEscape(keys[k]) + "=" + http.URLEscape(v.Get(keys[k]))
+    out += escape(keys[k]) + "=" + escape(v.Get(keys[k]))
   }
   return
 }
+
+func escapeTest(b byte)(out bool){
+  switch b {
+    case 'a','b','c','d','e','f','g','h','i','j','k','l','m',
+         'A','B','C','D','E','F','G','H','I','J','K','L','M',
+         'n','o','p','q','r','s','t','u','v','w','x','y','z',
+         'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+         '0','1','2','3','4','5','6','7','8','9','-','.','_':
+      out = false
+    default:
+      out = true
+  }
+  return
+}
+
+func escape(in string)(out string){
+  return urltools.Escape(in, escapeTest, urltools.PercentUpper)
+}
+
