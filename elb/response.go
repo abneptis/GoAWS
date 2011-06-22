08fda676259103e1108bebbdb81d7e87e0ca23f3
James D. Nurmi <james@abneptis.com>
2011-06-21 17:19:51 -0700

    Update ELB support to new results.

---
+++ elb/response.go
@@ -0,0 +1,19 @@
+package elb
+
+type LoadBalancerDescription struct {
+  CanonicalHostedZoneName string
+  CanonicalHostedZoneNameID string
+  CreatedTime string
+  LoadBalancerName string
+  SourceSecurityGroupOwnerAlias string "sourcesecuritygroup>owneralias"
+  SourceSecurityGroupGroupName string "sourcesecuritygroup>groupname"
+  DNSName string
+  Listeners []Listener "listenerdescriptions>member>listener"
+  AvailabilityZones []string "AvailabilityZones>member"
+}
+
+type LoadBalancerQueryResult struct {
+  LoadBalancerDescription []LoadBalancerDescription "DescribeLoadBalancersResult>LoadBalancerDescriptions>member"
+  RequestId string "requestmetadata>requestid"
+  ErrorCode string "error>errorcode"
+}
